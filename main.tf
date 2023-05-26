45c60d1ff57af8404e9670ce6246cb3f983e6eab
Osvaldo <osvaldo.tulini@gmail.com>
2023-05-26 11:07:52 +0200

    refs sparkfabrik-innovation-team/board#2244 - Update prometheus-stack module to use different passwd

--- main.tf
+++ main.tf
@@ -1,17 +1,24 @@
 locals {
-  app_name = "kube-prometheus-stack"
-  adapter_app_name = "prometheus-adapter"
-  prometheus_service_name = "kube-prometheus-stack-prometheus"
-  cert_manager_secret_name = trimspace(var.grafana_tls_secret_name) != "" ? var.grafana_tls_secret_name : join("-", [ local.app_name, "certmanager-def.tls" ])
+  app_name                 = "kube-prometheus-stack"
+  adapter_app_name         = "prometheus-adapter"
+  prometheus_service_name  = "kube-prometheus-stack-prometheus"
+  cert_manager_secret_name = trimspace(var.grafana_tls_secret_name) != "" ? var.grafana_tls_secret_name : join("-", [local.app_name, "certmanager-def.tls"])
 }
 
 resource "kubernetes_namespace" "kube_prometheus_stack_namespace" {
+  count = var.create_namespace ? 1 : 0
   metadata {
     name = var.namespace
     labels = {
       name = var.namespace
     }
   }
+}
+
+resource "random_password" "basic_auth_password" {
+  length           = 30
+  special          = true
+  override_special = "_%@"
 }
 
 resource "random_password" "grafana_admin_password" {
@@ -27,50 +34,50 @@ resource "kubernetes_secret" "kube_prometheus_ingress_auth" {
     name      = "${local.app_name}-basic-auth"
     namespace = var.namespace
     labels = {
-      app       = local.app_name
+      app = local.app_name
     }
   }
   data = {
     username = var.grafana_ingress_basic_auth_username
-    password = random_password.grafana_admin_password.result
-    auth = "${var.grafana_ingress_basic_auth_username}:{PLAIN}${random_password.grafana_admin_password.result}"
+    password = random_password.basic_auth_password.result
+    auth     = "${var.grafana_username}:{PLAIN}${random_password.grafana_admin_password.result}"
   }
 
-  depends_on = [ resource.kubernetes_namespace.kube_prometheus_stack_namespace ]
+  depends_on = [resource.kubernetes_namespace.kube_prometheus_stack_namespace]
 }
 
 data "template_file" "kube_prometheus_stack_config" {
   template = templatefile(
     "${path.module}/values/kube-prometheus-stack.yml",
     {
-      regcred = var.regcred
-      grafana_ingress_host = var.grafana_ingress_host
-      grafana_ingress_class = var.grafana_ingress_class
-      grafana_cluster_issuer_name = var.grafana_cluster_issuer_name
-      cert_manager_secret_name = local.cert_manager_secret_name
+      regcred                             = var.regcred
+      grafana_ingress_host                = var.grafana_ingress_host
+      grafana_ingress_class               = var.grafana_ingress_class
+      grafana_cluster_issuer_name         = var.grafana_cluster_issuer_name
+      cert_manager_secret_name            = local.cert_manager_secret_name
       grafana_ingress_basic_auth_username = var.grafana_ingress_basic_auth_username
-      grafana_ingress_basic_auth_message = var.grafana_ingress_basic_auth_message
-      grafana_ingress_basic_auth_secret = trimspace(var.grafana_ingress_basic_auth_username) != "" ? "${var.namespace}/${kubernetes_secret.kube_prometheus_ingress_auth[0].metadata[0].name}" : ""
+      grafana_ingress_basic_auth_message  = var.grafana_ingress_basic_auth_message
+      grafana_ingress_basic_auth_secret   = trimspace(var.grafana_ingress_basic_auth_username) != "" ? "${var.namespace}/${kubernetes_secret.kube_prometheus_ingress_auth[0].metadata[0].name}" : ""
     }
   )
 }
 
 resource "helm_release" "kube_prometheus_stack" {
-  name = local.app_name
-  repository = "https://prometheus-community.github.io/helm-charts"
-  chart = "kube-prometheus-stack"
-  namespace = var.namespace
-  version = var.prometheus_stack_chart_version
+  name             = local.app_name
+  repository       = "https://prometheus-community.github.io/helm-charts"
+  chart            = "kube-prometheus-stack"
+  namespace        = var.namespace
+  version          = var.prometheus_stack_chart_version
   create_namespace = true
 
-  values = trimspace(var.prometheus_stack_additional_values) != "" ? [ data.template_file.kube_prometheus_stack_config.template, var.prometheus_stack_additional_values ] : [ data.template_file.kube_prometheus_stack_config.template ]
+  values = trimspace(var.prometheus_stack_additional_values) != "" ? [data.template_file.kube_prometheus_stack_config.template, var.prometheus_stack_additional_values] : [data.template_file.kube_prometheus_stack_config.template]
 
   set_sensitive {
-    name = "grafana.adminPassword"
+    name  = "grafana.adminPassword"
     value = random_password.grafana_admin_password.result
   }
 
-  depends_on = [ resource.kubernetes_secret.kube_prometheus_ingress_auth ]
+  depends_on = [resource.kubernetes_secret.kube_prometheus_ingress_auth]
 }
 
 data "template_file" "prometheus_adapter_config" {
@@ -85,11 +92,11 @@ data "template_file" "prometheus_adapter_config" {
 resource "helm_release" "prometheus_adapter" {
   count = trimspace(var.prometheus_adapter_chart_version) != "" ? 1 : 0
 
-  name = local.adapter_app_name
+  name       = local.adapter_app_name
   repository = "https://prometheus-community.github.io/helm-charts"
-  chart = "prometheus-adapter"
-  namespace = var.namespace
-  version = var.prometheus_adapter_chart_version
+  chart      = "prometheus-adapter"
+  namespace  = var.namespace
+  version    = var.prometheus_adapter_chart_version
 
-  values = trimspace(var.prometheus_adapter_additional_values) != "" ? [ data.template_file.prometheus_adapter_config.template, var.prometheus_adapter_additional_values ] : [ data.template_file.prometheus_adapter_config.template ]
+  values = trimspace(var.prometheus_adapter_additional_values) != "" ? [data.template_file.prometheus_adapter_config.template, var.prometheus_adapter_additional_values] : [data.template_file.prometheus_adapter_config.template]
 }
